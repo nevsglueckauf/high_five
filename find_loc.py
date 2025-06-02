216f1269075f88d3201340430b8c2c0b384f93f8
Sven Schrodt <sven@schrodt.club>
2025-06-02 13:09:56 +0200

    Cleaning Bot started via PoE

--- find_loc.py
+++ find_loc.py
@@ -6,24 +6,19 @@ from typing import Self
 class LocationFinder:
     """  Lokations- / Geoinformation zu verschiedenen Themen (Objectives) finden
     
-    
         Z.B: cty: 'Berlin' / obj: 'supermarkt' 
 
-        Unter Benutzung der Overpass API
+        Unter Benutzung der Overpass API (Aggregator verschiedener Datenquellen)
         SEE https://overpass-api.de/
     """
-    
-    tmeot = 120 # Timeout in Sekunden
-    
-    lst_rsp:requests.models.Response = None # Letze Response als Objekt
-    
-    # API: Aggregator verschiedener Datenquellen
-        
+    # Timeout in Sekunden
+    tmeot = 120
+    # Letze Response als Objekt
+    lst_rsp:requests.models.Response = None
+    # API Base URL 
     base_url = "https://overpass-api.de/api/interpreter"
     
-    
-    
-    # Anfrage an Overpass API
+    # Anfrage an Overpass API senden
     def snd_req(self, cty:str, obj:str)  -> Self:
         """ Sendet POST Request (mit JSON Payload) zur API
 
@@ -35,23 +30,20 @@ class LocationFinder:
             requests.models.Response
         """
         
-        
-        
         query = self.gen_bs_q(cty=cty, obj=obj)
         self.lst_rsp =  requests.post(self.base_url, data={"data": query}, timeout=self.tmeot)
         return self
     
-    
+    # Query generieren
     def gen_bs_q(self, cty:str, obj:str):
         """ Generiert die Anfrage für die Basis-Suche nach :
             
             - Ort (cty)  und 
             - Sachgebiet (obj)
             
-            als Overpass API/Overpass QL
+            als Overpass API/Overpass QL in der Pyload
             
             SEE https://wiki.openstreetmap.org/wiki/Overpass_API/Overpass_QL#Recurse_down_(%3E)
-        
         """
         
         return f"""
@@ -65,6 +57,7 @@ class LocationFinder:
         out center;
         """
 
+    # Datei lesen
     def ld(self, fn:str) -> str:
         """ Datei laden 
         Args:
@@ -77,6 +70,7 @@ class LocationFinder:
             tmp = f.read()
         return tmp
     
+    # Datei schreiben
     def sv(self, fn:str, dta:str)->Self:
         """ String-Inhalt in Datei speichern
 
