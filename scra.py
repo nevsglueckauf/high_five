e5d098858e1f1b687496e2b4ad0fd07723b54fe8
Sven Schrodt <sven@schrodt.club>
2025-06-02 14:12:24 +0200

    Beispiel eines Mini-Scrapers

--- scra.py
+++ scra.py
@@ -1,27 +1,81 @@
-# 
+# Beispiel eines sehr simplen scraping Tools
+#
 #
 import requests
 from typing import Self
 
+
 class Scrapy:
+
+    # Flag für "Trocken"-Layu - ohne HTTP Request
+    # Achtung: Um zu crawlen bitte auf False setzen
+    DRY_RUN:bool = True
+
+    # Template einer zubenutzenden URI
+    URI_TPL: str = "https://www.edeka.de/marktsuche.jsp#/?searchstring={}"
+
+    # Liste mit Städtenamen
+    cities: list = []
     
-    BASE_URI  = 'https://www.edeka.de/api/market-gateway?'
+    # Letzte Reponse als Objekt
+    resp:requests.models.Response
+     
+    tmeot:int = 180
     
-    SUBSIDIARY_PATH = 'marketId={}'
-    
-    CITY_Q = 'https://www.edeka.de/marktsuche.jsp#/?searchstring={}'    
-    
-    def __init__(self):
-        self.uri = self.BASE_URI + self.SUBSIDIARY_PATH    
-    
-    def get_sub(self, id:int) -> Self:
-        print(self.uri.format(id))
+    def __init__(self, dta: list = []):
+        self.cities = dta
+
+    def crawl(self):
+        if len(self.cities) == 0:
+            raise ValueError("Keine Daten vorhanden!")
+        for item in self.cities:
+            uri = self.URI_TPL.format(item)
+            if self.DRY_RUN:
+                print(uri)
+            else:
+                query = self.gen_bs_q(cty=item, obj='supermarket')
+                self.resp = requests.post(uri, data={"data": query}, timeout=self.tmeot)
+
+    def gen_bs_q(self, cty:str, obj:str) -> str:
+        """ Generiert die Anfrage für die Basis-Suche nach :
+            
+            - Ort (cty)  und 
+            - Sachgebiet (obj)
+            
+            als Overpass API/Overpass QL in der Pyload
+            
+            SEE https://wiki.openstreetmap.org/wiki/Overpass_API/Overpass_QL#Recurse_down_(%3E)
+        """
         
-        
+        return f"""
+        [out:json];
+        area["name"="{cty}"]->.searchArea;
+        (
+        node["shop"="{obj}"](area.searchArea);
+        way["shop"="{obj}"](area.searchArea);
+        relation["shop"="{obj}"](area.searchArea);
+        );
+        out center;
+        """
 
 
-    
+# Städte im Regierungsbezirk Arnsberg
+rbz_01 = [
+    "Arnsberg",
+    "Bochum",
+    "Dortmund",
+    "Ennepe-Ruhr-Kreis",
+    "Hagen",
+    "Hamm",
+    "Herne",
+    "Hochsauerlandkreis",
+    "Märkischer Kreis",
+    "Olpe",
+    "Siegen-Wittgenstein",
+    "Soest",
+    "Unna",
+]
 
-with open('dta/cities.txt', 'r') as file:
-    a = file.readlines()
+scraper = Scrapy(rbz_01)
 
+scraper.crawl()
