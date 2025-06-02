7f27c3d3f08039ded0bfb5c50486f6a72a72c726
Sven Schrodt <sven@schrodt.club>
2025-06-02 08:57:03 +0200

    Cleaning bot was here

--- find_loc.py
+++ find_loc.py
@@ -3,33 +3,49 @@ import requests
 
 
 class LocationFinder:
+    """  Finding location related data to different objectives 
+    
+        E.G: loc: 'Berlin' / objective: 'supermarkt' 
+
+    Returns:
+        _type_: _description_
+    """
+    
+    tmeot = 120
     
     
-    #exit()
     # Anfrage an Overpass API
-    def snd_req(self, cty:str, what:str) -> requests.models.Response:
-        
+    def snd_req(self, cty:str, obj:str) -> requests.models.Response:
+        """ Sending request to external API
+
+        Args:
+            cty (str): Village/City/Region/ etc.
+            obj (str): objective of interest 
+
+        Returns:
+            requests.models.Response: 
+        """
         # Aggretagor
         url = "https://overpass-api.de/api/interpreter"
         
-        query = self.gen_q(cty=cty, what=what)
-        return requests.post(url, data={"data": query})
+        query = self.gen_q(cty=cty, obj=obj)
+        return requests.post(url, data={"data": query}, timeout=self.tmeot)
     
-    def gen_q(self, cty:str, what:str):
+    def gen_q(self, cty:str, obj:str):
         return f"""
         [out:json];
         area["name"="{cty}"]->.searchArea;
         (
-        node["shop"="{what}"](area.searchArea);
-        way["shop"="{what}"](area.searchArea);
-        relation["shop"="{what}"](area.searchArea);
+        node["shop"="{obj}"](area.searchArea);
+        way["shop"="{obj}"](area.searchArea);
+        relation["shop"="{obj}"](area.searchArea);
         );
         out center;
         """
 
 foo = LocationFinder()
 
-dta = foo.snd_req(cty='Berlin', what='supermarket')
+dta = foo.snd_req(cty='Moers', obj='supermarket')
 
 print(dta.text)
 
--- suck_dta.py
+++
@@ -1,9 +0,0 @@
-
-uri = 'https://www.edeka.de/marktsuche.jsp#/?searchstring={}'
-
-with open('city_h.txt', 'r') as file:
-    a = file.readlines()
-
-for city in a:
-    cur_uri = uri.format(city.strip())
-    print(cur_uri)
