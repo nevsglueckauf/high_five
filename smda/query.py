7e1f33d658e26f039aeb5a6a3483842eed1aca5b
Sven Schrodt <sven@schrodt.club>
2025-06-03 12:30:12 +0200

    Adding Query class

---
+++ smda/query.py
@@ -0,0 +1,25 @@
+# Query collection
+
+class Query:
+    
+    loc_sm = """
+        [out:json];
+        area["name"="{}"]->.searchArea;
+        (
+        node["shop"="{}"](area.searchArea);
+        way["shop"="{}"](area.searchArea);
+        relation["shop"="{}"](area.searchArea);
+        );
+        out center;
+        """
+
+
+
+    def __init__(self):
+        pass
+    
+    def q_sm_cty(self, cty:str, obj:str='supermarket'):
+        
+        return self.loc_sm.format(cty, obj, obj, obj)
+    
+    
